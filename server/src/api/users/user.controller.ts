1e6a0c733dfbca747aefe14842c70aec63f78827
lightpurple <dmstjd10@gmail.com>
2023-08-23 16:32:45 +0900

    user controller 설정
    - 조회, 생성 기능 추가

--- server/src/api/users/user.controller.ts
+++ server/src/api/users/user.controller.ts
@@ -9,7 +9,7 @@ export default class UserController {
 
         try {
             const { rows, count } = await new UserService().list();
-            result = Result.ok<JSON>(count, rows).toJson();
+            result = Result.ok<any>({ rows, count }).toJson();
         } catch (e: any) {
             logger.err(JSON.stringify(e));
             logger.error(e);
@@ -21,4 +21,47 @@ export default class UserController {
         res.status(httpStatus.OK).json(result);
     };
 
+    get = async (req, res, next) => {
+        let result;
+
+        try {
+            const { id } = req.params;
+            const user = await new UserService().get(id);
+            result = Result.ok<any>(user).toJson();
+        } catch (e: any) {
+            logger.err(JSON.stringify(e));
+            logger.error(e);
+
+            result = Result.fail<Error>(e).toJson();
+        }
+
+        logger.res(httpStatus.OK, result, req);
+        res.status(httpStatus.OK).json(result);
+    };
+
+    create = async (req, res, next) => {
+        let result;
+
+        try {
+            const { name, email, password, role, region } = req.body;
+            const userData = {
+                name,
+                email,
+                password,
+                role,
+                region,
+            }
+
+            const user = await new UserService().create(userData);
+            result = Result.ok<any>(user).toJson();
+        } catch (e: any) {
+            logger.err(JSON.stringify(e));
+            logger.error(e);
+
+            result = Result.fail<Error>(e).toJson();
+        }
+
+        logger.res(httpStatus.OK, result, req);
+        res.status(httpStatus.OK).json(result);
+    }
 }
